3f982ba17e15b0c97da3f386cc1ff18823234e95
martin <ars.martin@hotmail.fr>
2016-06-07 10:51:51 +0200

    Martin code

--- src/model/pieces/PieceList.java
+++ src/model/pieces/PieceList.java
@@ -23,103 +23,217 @@ public class PieceList {
 		this.pieces = new ArrayList<int[][]>();	//The list of the pieces
 		setPiece0(1);		//The piece 0
 		setPiece1(1);		//The piece 1
+		setPiece2(1);		//The piece 2
+		setPiece3(1);		//The piece 3
+		setPiece4(1);		//The piece 4
+		setPiece5(1);		//The piece 5
+		setPiece6(1);		//The piece 6
+		setPiece7(1);		//The piece 7
+		setPiece8(1);		//The piece 8
+		setPiece9(1);		//The piece 9
+		setPiece10(1);		//The piece 10
+		setPiece11(1);		//The piece 11
+		setPiece12(1);		//The piece 12
+		setPiece13(1);		//The piece 13
+		setPiece14(1);		//The piece 14
+		setPiece15(1);		//The piece 15
+		setPiece16(1);		//The piece 16
+		setPiece17(1);		//The piece 17
+		setPiece18(1);		//The piece 18
+		setPiece19(1);		//The piece 19
+		setPiece20(1);		//The piece 20
 	}
 
-
+	/**
+	 * This method allows to create the piece 0
+	 * @param position the position of the piece
+	 */
 	public void setPiece0(int position) {
 		Piece0 piece0 = new Piece0(this.color);
 		this.pieces.add(0, piece0.getPiece0Position(position));
 	}
 
+	/**
+	 * This method allows to create the piece 1
+	 * @param position the position of the piece
+	 */
 	public void setPiece1(int position) {
 		Piece1 piece1 = new Piece1(this.color);
 		this.pieces.add(1, piece1.getPiece1Position(position));
 	}
 
-		/*//The piece 0
-		Piece0 piece0 = new Piece0(color);
-		this.pieces.add(piece0.getPiece0Position(1));
+	/**
+	 * This method allows to create the piece 2
+	 * @param position the position of the piece
+	 */
+	public void setPiece2(int position) {
+		Piece2 piece2 = new piece2(this.color);
+		this.pieces.add(2, piece2.getPiece2Position(position));
+	}
 
-		//The piece 1
-		Piece1 piece1 = new Piece1(color);
-		this.pieces.add(piece1.getPiece1Position(1));
+	/**
+	 * This method allows to create the piece 3
+	 * @param position the position of the piece
+	 */
+	public void setPiece3(int position) {
+		Piece3 piece3 = new Piece3(this.color);
+		this.pieces.add(3, piece3.getPiece3Position(position));
+	}
 
-		//The piece 2
-		Piece2 piece2 = new Piece2(color);
-		this.pieces.add(piece2.getPiece2Position(1));
+	/**
+	 * This method allows to create the piece 4
+	 * @param position the position of the piece
+	 */
+	public void setPiece4(int position) {
+		Piece4 piece4 = new Piece4(this.color);
+		this.pieces.add(4, piece4.getPiece4Position(position));
+	}
 
-		//The piece 3
-		Piece3 piece3 = new Piece3(color);
-		this.pieces.add(piece3.getPiece3Position(1));
+	/**
+	 * This method allows to create the piece 5
+	 * @param position the position of the piece
+	 */
+	public void setPiece5(int position) {
+		Piece5 piece5 = new Piece5(this.color);
+		this.pieces.add(5, piece5.getPiece5Position(position));
+	}
 
-		//The piece 4
-		Piece4 piece4 = new Piece4(color);
-		this.pieces.add(piece4.getPiece4Position(1));
+	/**
+	 * This method allows to create the piece 6
+	 * @param position the position of the piece
+	 */
+	public void setPiece6(int position) {
+		Piece6 piece6 = new Piece6(this.color);
+		this.pieces.add(6, piece6.getPiece6Position(position));
+	}
 
-		//The piece 5
-		Piece5 piece5 = new Piece5(color);
-		this.pieces.add(piece5.getPiece5Position(1));
+	/**
+	 * This method allows to create the piece 7
+	 * @param position the position of the piece
+	 */
+	public void setPiece7(int position) {
+		Piece7 piece7 = new Piece7(this.color);
+		this.pieces.add(7, piece7.getPiece7Position(position));
+	}
 
-		//The piece 6
-		Piece6 piece6 = new Piece6(color);
-		this.pieces.add(piece6.getPiece6Position(1));
+	/**
+	 * This method allows to create the piece 8
+	 * @param position the position of the piece
+	 */
+	public void setPiece8(int position) {
+		Piece8 piece8 = new Piece8(this.color);
+		this.pieces.add(8, piece8.getPiece8Position(position));
+	}
 
-		//The piece 7
-		Piece7 piece7 = new Piece7(color);
-		this.pieces.add(piece7.getPiece7Position(1));
+	/**
+	 * This method allows to create the piece 9
+	 * @param position the position of the piece
+	 */
+	public void setPiece9(int position) {
+		Piece9 piece9 = new Piece9(this.color);
+		this.pieces.add(9, piece9.getPiece9Position(position));
+	}
 
-		//The piece 8
-		Piece8 piece8 = new Piece8(color);
-		this.pieces.add(piece8.getPiece8Position(1));
+	/**
+	 * This method allows to create the piece 10
+	 * @param position the position of the piece
+	 */
+	public void setPiece10(int position) {
+		Piece10 piece10 = new Piece10(this.color);
+		this.pieces.add(10, piece10.getPiece10Position(position));
+	}
 
-		//The piece 9
-		Piece9 piece9 = new Piece9(color);
-		this.pieces.add(piece9.getPiece9Position(1));
+	/**
+	 * This method allows to create the piece 11
+	 * @param position the position of the piece
+	 */
+	public void setPiece11(int position) {
+		Piece11 piece11 = new Piece11(this.color);
+		this.pieces.add(11, piece11.getPiece11Position(position));
+	}
 
-		//The piece 10
-		Piece10 piece10 = new Piece10(color);
-		this.pieces.add(piece10.getPiece10Position(1));
+	/**
+	 * This method allows to create the piece 12
+	 * @param position the position of the piece
+	 */
+	public void setPiece12(int position) {
+		Piece12 piece12 = new Piece12(this.color);
+		this.pieces.add(12, piece12.getPiece12Position(position));
+	}
 
-		//The piece 11
-		Piece11 piece11 = new Piece11(color);
-		this.pieces.add(piece11.getPiece11Position(1));
+	/**
+	 * This method allows to create the piece 13
+	 * @param position the position of the piece
+	 */
+	public void setPiece13(int position) {
+		Piece13 piece13 = new Piece13(this.color);
+		this.pieces.add(13, piece13.getPiece13Position(position));
+	}
 
-		//The piece 12
-		Piece12 piece12 = new Piece12(color);
-		this.pieces.add(piece12.getPiece12Position(1));
+	/**
+	 * This method allows to create the piece 14
+	 * @param position the position of the piece
+	 */
+	public void setPiece14(int position) {
+		Piece14 piece14 = new Piece14(this.color);
+		this.pieces.add(14, piece14.getPiece14Position(position));
+	}
 
-		//The piece 13
-		Piece13 piece13 = new Piece13(color);
-		this.pieces.add(piece13.getPiece13Position(1));
+	/**
+	 * This method allows to create the piece 15
+	 * @param position the position of the piece
+	 */
+	public void setPiece15(int position) {
+		Piece15 piece15 = new Piece15(this.color);
+		this.pieces.add(15, piece15.getPiece15Position(position));
+	}
 
-		//The piece 14
-		Piece14 piece14 = new Piece14(color);
-		this.pieces.add(piece14.getPiece14Position(1));
+	/**
+	 * This method allows to create the piece 16
+	 * @param position the position of the piece
+	 */
+	public void setPiece16(int position) {
+		Piece16 piece16 = new Piece16(this.color);
+		this.pieces.add(16, piece16.getPiece16Position(position));
+	}
 
-		//The piece 15
-		Piece15 piece15 = new Piece15(color);
-		this.pieces.add(piece15.getPiece15Position(1));
+	/**
+	 * This method allows to create the piece 17
+	 * @param position the position of the piece
+	 */
+	public void setPiece17(int position) {
+		Piece17 piece17 = new Piece17(this.color);
+		this.pieces.add(17, piece17.getPiece17Position(position));
+	}
 
-		//The piece 16
-		Piece16 piece16 = new Piece16(color);
-		this.pieces.add(piece16.getPiece16Position(1));
+	/**
+	 * This method allows to create the piece 18
+	 * @param position the position of the piece
+	 */
+	public void setPiece18(int position) {
+		Piece18 piece18 = new Piece18(this.color);
+		this.pieces.add(18, piece18.getPiece18Position(position));
+	}
 
-		//The piece 17
-		Piece17 piece17 = new Piece17(color);
-		this.pieces.add(piece17.getPiece17Position(1));
+	/**
+	 * This method allows to create the piece 19
+	 * @param position the position of the piece
+	 */
+	public void setPiece19(int position) {
+		Piece19 piece19 = new Piece19(this.color);
+		this.pieces.add(19, piece19.getPiece19Position(position));
+	}
 
-		//The piece 18
-		Piece18 piece18 = new Piece18(color);
-		this.pieces.add(piece18.getPiece18Position(1));
+	/**
+	 * This method allows to create the piece 20
+	 * @param position the position of the piece
+	 */
+	public void setPiece20(int position) {
+		Piece20 piece20 = new Piece20(this.color);
+		this.pieces.add(20, piece20.getPiece20Position(position));
+	}
 
-		//The piece 19
-		Piece19 piece19 = new Piece19(color);
-		this.pieces.add(piece19.getPiece19Position(1));
-
-		//The piece 20
-		Piece20 piece20 = new Piece20(color);
-		this.pieces.add(piece20.getPiece20Position(1));
-	}*/
+////////////////////////////////////////////////////// GETLISTPIECE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 	/**
 	 * This method allows to return the ArrayListof the pieces
@@ -132,7 +246,7 @@ public class PieceList {
 	public static void main(String[] args) {
 		PieceList p = new PieceList(1);
 
-		int[][] oui = p.getListPiece().get(1);
+		int[][] oui = p.getListPiece().get(12);
 		for(int i=0; i < oui.length; i++) {
 			for(int j=0; j < oui.length; j++) {
 				System.out.print(oui[i][j]);
@@ -144,7 +258,7 @@ public class PieceList {
 
 		System.out.println("");
 
-		oui = p.getListPiece().get(1);
+		oui = p.getListPiece().get(12);
 		for(int k=0; k < oui.length; k++) {
 			for(int n=0; n < oui.length; n++) {
 				System.out.print(oui[k][n]);
